0f33c5e396410f006619b169d8303606142dfa0a
Milton Pagan <milton.pagan1@upr.edu>
2021-10-04 22:21:43 -0400

    Add base server code

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import mysql from "mysql";
+import { Sequelize } from "sequelize";
+import rumergy from "./api/routes/rumergy.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/rumergy", rumergy);
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+
+export default app;

fd2142820c8ce359897dc7ee2a0cbb7a4b96bf0b
Milton Pagan <milton.pagan1@upr.edu>
2021-10-04 22:23:42 -0400

    Add user model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import mysql from "mysql";
+import mysql2 from "mysql2";
 import { Sequelize } from "sequelize";
 import rumergy from "./api/routes/rumergy.route.js";
 
@@ -11,6 +11,5 @@ app.use(express.json());
 
 app.use("/api/rumergy", rumergy);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
-
 
 export default app;

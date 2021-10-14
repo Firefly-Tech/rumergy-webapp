fb96be120800e4a0e791cd20e0c739fbcaf9d76e
Milton Pagan <milton.pagansoto@gmail.com>
2021-10-13 22:43:03 -0400

    Finish base sidebar implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import Sidebar from "./components/sidebar.component";
 
 function App() {
   return (
-      <Router>
-        <Container fluid className="main-container">
-          <Row>
-            <Col xs={2} className="sidebar-wrapper">
-              <Sidebar userRole="GEN" />
-            </Col>
-            <Col xs={10} id="page-content">
-              <Route path="/dashboard"><span>Dashboard</span></Route>
-            </Col>
-          </Row>
-        </Container>
-      </Router>
+    <Router>
+      <Sidebar userRole="GEN" />
+      <Route path="/" exact>
+        <Redirect to="/dashboard" />
+      </Route>
+      <Container fluid className="main-container">
+        <Row>
+          <Col xs={10} id="page-content">
+            <Route path="/dashboard">
+              <span>Dashboard</span>
+            </Route>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
--- frontend/src/components/sidebar.component.js
+++ frontend/src/components/sidebar.component.js
@@ -1,9 +1,18 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { Nav } from "react-bootstrap";
-import SidebarItem from "./sidebarItem.component";
 import { roles } from "../resources/constants";
-import { FaChartLine, FaBolt } from "react-icons/fa";
+import {
+  FaChartLine,
+  FaBolt,
+  FaInfoCircle,
+  FaSignInAlt,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+import Help from "./help.component";
+
+// TODO: Remove sidebar from container
 
 const sidebarData = [
   {
@@ -13,38 +22,58 @@ const sidebarData = [
     icon: <FaChartLine />,
   },
   {
-    menuName: "Dashboard",
-    link: "/dashboard",
+    menuName: "About",
+    link: "/about",
     userRestrictions: [roles.General, roles.Advanced],
-    icon: <FaChartLine />,
+    icon: <FaInfoCircle />,
   },
 ];
 
 function Sidebar(props) {
+  const location = useLocation();
+
   const checkRestrictions = (restrictions) => {
     return restrictions.includes(props.userRole);
   };
 
+  const isLoggedIn = () => {
+    return props.userRole !== roles.General;
+  };
+
   return (
-    <div className="sidebar flex-column">
+    <div className="sidebar">
+      {/* Header */}
       <div className="sidebar-header">
         <FaBolt />
         <h2>RUMergy</h2>
       </div>
-      <Nav className="flex-column nav-container">
+      {/* Nav */}
+      <Nav className="flex-column nav-container" activeKey={location.pathname}>
         {sidebarData.map((item, index) => {
           return (
             checkRestrictions(item.userRestrictions) && (
-              <SidebarItem
-                menuName={item.menuName}
-                link={item.link}
-                icon={item.icon}
-                key={index}
-              />
+              <Nav.Item className="sidebar-item">
+                <Nav.Link href={item.link}>
+                  {item.icon}
+                  <span id="sidebar-menu-span">{item.menuName}</span>
+                </Nav.Link>
+              </Nav.Item>
             )
           );
         })}
       </Nav>
+      {/* Footer */}
+      <div className="sidebar-footer">
+        <div className="sidebar-login-logout">
+          <Link to={isLoggedIn() ? "/logout" : "/login"}>
+            {isLoggedIn() ? <FaSignOutAlt /> : <FaSignInAlt />}
+            <span>{isLoggedIn() ? "Logout" : "Login"}</span>
+          </Link>
+        </div>
+        <div className="sidebar-help">
+          <Help />
+        </div>
+      </div>
     </div>
   );
 }

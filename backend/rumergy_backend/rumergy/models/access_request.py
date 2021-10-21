2919d788e193fc89b17dcb95ca69ddf1492bed76
Milton Pagan <milton.pagansoto@gmail.com>
2021-10-20 20:52:35 -0400

    RUME-231 RUME-232 Add data log measures and access request models

--- backend/rumergy_backend/rumergy/models/access_request.py
+++ backend/rumergy_backend/rumergy/models/access_request.py
@@ -1,5 +1,11 @@
 from django.db import models
 from . import User
 
+
 class AccessRequest(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    """Access request model"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ocupation = models.CharField(max_length=20)
+    justification = modles.CharField(max_length=200)

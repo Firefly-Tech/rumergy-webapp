b55ca95a81b4ed2ee7666acf00db6d705aca0844
Milton Pagan <milton.pagansoto@gmail.com>
2021-10-28 09:37:50 -0400

    Fix timeframe bug

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -12,14 +12,18 @@ const testMeterList = [
 ];
 
 const testData = {
-  labels: ["1", "2", "3", "4", "5", "6"],
+  labels: [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ],
   datasets: [
     {
       label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
+      data: [
+        12, 19, 3, 5, 2, 3, 12, 19, 32, 5, 2, 3, 12, 198, 3, 5, 2, 3, 2, 3,
+      ],
       fill: false,
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgba(255, 99, 132, 0.2)",
+      backgroundColor: "rgb(0, 79, 45)",
+      borderColor: "rgba(0, 79, 45, 0.2)",
     },
   ],
 };
@@ -30,7 +34,8 @@ function Dashboard(props) {
   const [meterNames, setMeterNames] = useState([]);
   const [meterData, setMeterData] = useState({});
   const [meterIDList, setMeterIDList] = useState([]);
-  const [selectedTimeframe, setSelectedTimeframe] = useState();
+  const [selectedTimeframe, setSelectedTimeframe] =
+    useState(DAY_IN_MILLISECONDS);
   const [selectedDatatype, setSelectedDatatype] = useState("consumption");
   const [selectedMeters, setSelectedMeters] = useState([]);
 
@@ -86,7 +91,7 @@ function Dashboard(props) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  /* METER DATA FETCH HANDLERS */
+  /* METER DATA FETCH HANDLERS/HELPERS*/
   const handleFetch = () => {
     if (!(selectedMeters.length > 0)) {
       handleShow();
@@ -99,19 +104,42 @@ function Dashboard(props) {
   const fetchData = () => {
     setLoading(true);
 
+    const getLabel = () => {
+      return selectedDatatype === "consumption" ? "Consumption" : "Demand";
+    };
+
     var startingDateTime = new Date(
       Date.now() - selectedTimeframe
     ).toISOString();
 
+    // TODO: Add axis labels
+    const data = {
+      labels: [],
+      datasets: [
+        {
+          label: getLabel(),
+          data: [],
+          fill: false,
+          backgroundColor: "rgb(0, 79, 45)",
+          borderColor: "rgba(0, 79, 45, 0.2)",
+        },
+      ],
+    };
+
     if (meterIDList.length > 1) {
       // Multiple meters
-
-
+      //consumption
+      //demand
     } else {
       // Single meter
+      //consumption
+      //demand
     }
 
-    setMeterData(testData);
+    data.labels = testData.labels;
+    data.datasets[0].data = testData.datasets[0].data;
+
+    setMeterData(data);
     setLoading(false);
   };
 

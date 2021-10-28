5fbcd4484acaaf0b532f8520d5ddf67e89db0a46
Milton Pagan <milton.pagansoto@gmail.com>
2021-10-28 17:52:34 -0400

    Add cors

--- backend/rumergy_backend/settings.py
+++ backend/rumergy_backend/settings.py
@@ -29,9 +29,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -132,3 +134,5 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
+
+CORS_ORIGIN_ALLOW_ALL = True

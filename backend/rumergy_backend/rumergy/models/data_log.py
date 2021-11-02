85a2536b1ffeb622cd1e0c0f1017e62e6863aaf0
sebastianmerced <sebastian.merced@upr.edu>
2021-11-01 22:06:49 -0400

    Added date defaults for data log model.

--- backend/rumergy_backend/rumergy/models/data_log.py
+++ backend/rumergy_backend/rumergy/models/data_log.py
@@ -1,10 +1,11 @@
 from django.db import models
 from . import UserProfile
 # from . import Meter
+from django.utils.timezone import now
 
 
 class DataLog(models.Model):
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
+    start_date = models.DateTimeField(default=now)
+    end_date = models.DateTimeField(default=now)
     # meter = models.ForeignKey(Meter, related_name='data_logs', on_delete=models.PROTECT)
     user = models.ForeignKey(UserProfile, related_name='data_logs', on_delete=models.PROTECT)

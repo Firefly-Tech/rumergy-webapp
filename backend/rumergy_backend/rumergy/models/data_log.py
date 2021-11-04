04af069033eb1948335c5d4353df30f3359f2ae9
sebastianmerced <sebastian.merced@upr.edu>
2021-11-04 13:53:45 -0400

    Imported the correct User model to data_log file

--- backend/rumergy_backend/rumergy/models/data_log.py
+++ backend/rumergy_backend/rumergy/models/data_log.py
@@ -1,5 +1,5 @@
 from django.db import models
-from . import UserProfile
+from django.contrib.auth.models import User
 # from . import Meter
 from django.utils.timezone import now
 
@@ -8,4 +8,6 @@ class DataLog(models.Model):
     start_date = models.DateTimeField(default=now)
     end_date = models.DateTimeField(default=now)
     # meter = models.ForeignKey(Meter, related_name='data_logs', on_delete=models.PROTECT)
-    user = models.ForeignKey(UserProfile, related_name='data_logs', on_delete=models.PROTECT)
+    user = models.ForeignKey(User, related_name='data_logs', on_delete=models.PROTECT)
+
+# TODO Import Meter model

351297fa72116442212d47cb08a6454767ca7427
sebastianmerced <sebastian.merced@upr.edu>
2021-11-05 15:27:36 -0400

    Deleted model from data_point_serializer

--- backend/rumergy_backend/rumergy/serializers/data_point_serializer.py
+++ backend/rumergy_backend/rumergy/serializers/data_point_serializer.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
 from rumergy_backend.rumergy.models.data_point import DataPoint
-# from rumergy_backend.rumergy.models.model import Model
 
 class DataPointSerializer(serializers.ModelSerializer):
     
-    # model = serializers.PrimaryKeyRelatedField(queryset=Model.objects.all())
-    # TODO import Model model
     class Meta:
         model = DataPoint
-        fields = ['name', 'unit', 'start_address', 'end_address', 'data_type', 'register_type', 'model']
+        fields = ['name', 'unit', 'start_address', 'end_address', 'data_type', 'register_type']
+   

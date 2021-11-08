e40760f17b8d55be9176901cabd9580066e27677
Milton Pagan <milton.pagansoto@gmail.com>
2021-11-07 20:11:59 -0400

    Add meter data by timeframe endpoint to meter view
    Also added str methods to some models and added models to admin site
    for easier testing.

--- backend/rumergy_backend/rumergy/admin.py
+++ backend/rumergy_backend/rumergy/admin.py
@@ -4,3 +4,8 @@ from rumergy_backend.rumergy import models
 # Register your models here.
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.Meter)
+admin.site.register(models.MeterModel)
+admin.site.register(models.DataPoint)
+admin.site.register(models.Building)
+admin.site.register(models.MeterData)

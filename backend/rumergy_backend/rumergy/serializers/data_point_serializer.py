06cf484257964323459079b7ad823060e5722dbc
Milton Pagan <milton.pagansoto@gmail.com>
2021-11-11 14:31:16 -0400

    Revamp data log and datapoint relationships

--- backend/rumergy_backend/rumergy/serializers/data_point_serializer.py
+++ backend/rumergy_backend/rumergy/serializers/data_point_serializer.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 from rumergy_backend.rumergy.models.data_point import DataPoint
 
+
 class DataPointSerializer(serializers.ModelSerializer):
-    
+
+    data_logs = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = DataPoint
-        fields = ["id", "model", "name", "unit", "start_address", "end_address", "data_type", "register_type"]
-   
+        fields = [
+            "id",
+            "model",
+            "name",
+            "unit",
+            "start_address",
+            "end_address",
+            "data_type",
+            "register_type",
+            "data_logs",
+        ]
+

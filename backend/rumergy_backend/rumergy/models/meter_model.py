d3fa22cd4b40c19a2dbfde015308330009a6a031
Orlando Ruiz <orlando.ruiz5@upr.edu>
2021-11-11 10:44:43 -0800

    Add unique constraints to some variables

--- backend/rumergy_backend/rumergy/models/meter_model.py
+++ backend/rumergy_backend/rumergy/models/meter_model.py
@@ -4,7 +4,7 @@ from django.db import models
 class MeterModel(models.Model):
     """Database model for meter models in the system"""
 
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, unique=True)
 
     def __str__(self):
         """Return string representation of the meter model"""

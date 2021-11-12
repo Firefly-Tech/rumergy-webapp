bd6a67cebc0fe0143c860c452b4f73c17e9306bb
Milton Pagan <milton.pagansoto@gmail.com>
2021-11-11 21:48:21 -0400

    Add user edit and delete

--- frontend/src/components/ManageUsers.jsx
+++ frontend/src/components/ManageUsers.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import { useRequireAuth } from "../resources/use-require-auth";
 import { roles } from "../resources/constants";
 import {
@@ -10,13 +11,47 @@ import {
   Card,
   InputGroup,
   FormControl,
+  Form,
 } from "react-bootstrap";
-import DataTable, { createTheme } from "react-data-table-component";
+import DataTable from "react-data-table-component";
 import IconButton from "./IconButton";
-import { FaPen, FaRedo, FaPlus, FaTimes } from "react-icons/fa";
-import SearchBar from "./SearchBar";
+import {
+  FaPen,
+  FaRedo,
+  FaPlus,
+  FaTimes,
+  FaSync,
+  FaTrash,
+  FaExclamation,
+  FaCheck,
+} from "react-icons/fa";
 import { Formik } from "formik";
 import * as Yup from "yup";
+
+const userEditFormSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(1, "Must be at least 1 character")
+    .max(50, "Must be at most 50 characters")
+    .required("First name is required")
+    .matches(/\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/, "Invalid format"),
+  lastName: Yup.string()
+    .min(1, "Must be at least 1 character")
+    .max(50, "Must be at most 50 characters")
+    .required("Last name is required")
+    .matches(/\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/, "Invalid format"),
+  email: Yup.string().email("Invalid email format").required("Email required"),
+  role: Yup.string().required("Role required").oneOf(Object.values(roles)),
+});
+
+const emptyEditUserEntry = {
+  id: null,
+  profile: {
+    first_name: "",
+    last_name: "",
+    role: "",
+  },
+  email: "",
+};
 
 function ManageUsers() {
   const [loading, setLoading] = useState(false);
@@ -25,36 +60,17 @@ function ManageUsers() {
   const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
   const [filteredEntries, setFilteredEntries] = useState([]);
 
-  const [selectedEntry, setSelectedEntry] = useState(null);
+  // Modal
+  const [selectedEditEntry, setSelectedEditEntry] =
+    useState(emptyEditUserEntry);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const auth = useRequireAuth("/login", [roles.Admin]);
 
-  const getUsers = async () => {
-    return auth.userAxiosInstance
-      .get(`${auth.apiHost}/api/users`)
-      .then((res) => {
-        return res.data;
-      })
-      .catch((error) => {
-        return [];
-      });
-  };
-
   useEffect(async () => {
-    setLoading(true);
-    let data = await getUsers();
-    if (!data.length) setUsers(data);
-    setUsers(
-      data.map((user) => {
-        let userString = `${user.id}${user.username}${user.email}${user.profile.first_name}${user.profile.last_name}${user.profile.role}`;
-
-        return { userString: userString, ...user };
-      })
-    );
-    setLoading(false);
+    await fetchUsers();
   }, []);
 
   useEffect(() => {
@@ -67,6 +83,27 @@ function ManageUsers() {
     setLoading(false);
   }, [users, filterText]);
 
+  const fetchUsers = async () => {
+    setLoading(true);
+    let data = await auth.userAxiosInstance
+      .get(`${auth.apiHost}/api/users`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch(() => {
+        return [];
+      });
+    if (!data.length) setUsers(data);
+    setUsers(
+      data.map((user) => {
+        let userString = `${user.id}${user.username}${user.email}${user.profile.first_name}${user.profile.last_name}${user.profile.role}`;
+
+        return { userString: userString, ...user };
+      })
+    );
+    setLoading(false);
+  };
+
   const columns = [
     {
       name: "Action",
@@ -74,7 +111,7 @@ function ManageUsers() {
         <IconButton
           icon={<FaPen className="fs-5" />}
           clickAction={() => {
-            setSelectedEntry(row);
+            setSelectedEditEntry(row);
             handleShow();
           }}
         />
@@ -126,6 +163,48 @@ function ManageUsers() {
     }
   };
 
+  const handleEdit = async (id, values, { setSubmitting }) => {
+    setLoading(true);
+    let data = {
+      profile: {
+        first_name: values.firstName,
+        last_name: values.lastName,
+        role: values.role,
+      },
+      email: values.email,
+    };
+    return auth.userAxiosInstance
+      .patch(`${auth.apiHost}/api/users/${id}/`, data)
+      .then(() => {
+        fetchUsers();
+        return true;
+      })
+      .catch(() => {
+        return false;
+      })
+      .finally(() => {
+        setSubmitting(false);
+        setLoading(false);
+      });
+  };
+
+  const handleDelete = (id, { setSubmitting }) => {
+    setLoading(true);
+    return auth.userAxiosInstance
+      .delete(`${auth.apiHost}/api/users/${id}/`)
+      .then(() => {
+        fetchUsers();
+        return true;
+      })
+      .catch(() => {
+        return false;
+      })
+      .finally(() => {
+        setSubmitting(false);
+        setLoading(false);
+      });
+  };
+
   return (
     <>
       {/* HEADER */}
@@ -165,20 +244,20 @@ function ManageUsers() {
                 <div className="d-flex flex-row align-items-center px-4 gap-4">
                   <IconButton
                     icon={<FaRedo className="fs-5" />}
-                    clickAction={() => {}}
+                    clickAction={() => {
+                      fetchUsers();
+                    }}
                   />
                   <Button
                     variant="primary"
                     className="d-flex flex-row align-items-center gap-3"
                     onClick={() => {
-                      setSelectedEntry(null);
+                      setSelectedEditEntry(null);
                       handleShow();
                     }}
                   >
                     <FaPlus className="fs-5" />
-                    <span className="d-none d-sm-inline">
-                      {"Add new meter"}
-                    </span>
+                    <span className="d-none d-sm-inline">{"Add new user"}</span>
                   </Button>
                 </div>
               </Card>
@@ -199,9 +278,254 @@ function ManageUsers() {
           </Row>
         </Col>
       </Row>
-      <Modal centerd size="lg" show={show} onHide={handleClose}></Modal>
+      <UserEditModal
+        show={show}
+        handleClose={handleClose}
+        selectedEditEntry={selectedEditEntry}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
     </>
   );
 }
+
+function UserEditModal(props) {
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const resetAll = () => {
+    setIsUpdate(false);
+    setIsDelete(false);
+    setSuccess(false);
+    setError(false);
+  };
+
+  return (
+    <Modal
+      centered
+      size="lg"
+      show={props.show}
+      onHide={() => {
+        props.handleClose();
+        resetAll();
+      }}
+      backdrop="static"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <h4 className="bold">Edit Entry</h4>
+        </Modal.Title>
+      </Modal.Header>
+      <Formik
+        initialValues={{
+          firstName: props.selectedEditEntry.profile.first_name,
+          lastName: props.selectedEditEntry.profile.last_name,
+          email: props.selectedEditEntry.email,
+          role: props.selectedEditEntry.profile.role,
+        }}
+        validationSchema={userEditFormSchema}
+        onSubmit={async (values, handlers) => {
+          let status;
+          if (isUpdate) {
+            status = await props.handleEdit(
+              props.selectedEditEntry.id,
+              values,
+              handlers
+            );
+          } else {
+            console.log("delete");
+            status = await props.handleDelete(
+              props.selectedEditEntry.id,
+              handlers
+            );
+          }
+          if (status) setSuccess(true);
+          else setError(true);
+        }}
+        enableReinitialize
+      >
+        {(formik) => (
+          <>
+            <Form
+              onSubmit={formik.handleSubmit}
+              noValidate
+              className="d-flex flex-column"
+            >
+              <Modal.Body>
+                <Form.Group className="mb-3">
+                  <Form.Label>First Name</Form.Label>
+                  <InputGroup hasValidation>
+                    <Form.Control
+                      id="firstName"
+                      placeholder="Enter first name"
+                      isInvalid={!!formik.errors.firstName}
+                      {...formik.getFieldProps("firstName")}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.firstName}
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Last Name</Form.Label>
+                  <InputGroup hasValidation>
+                    <Form.Control
+                      id="lastName"
+                      placeholder="Enter last name"
+                      isInvalid={!!formik.errors.lastName}
+                      {...formik.getFieldProps("lastName")}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.lastName}
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Role</Form.Label>
+                  <InputGroup hasValidation>
+                    <Form.Select
+                      id="role"
+                      placeholder="Select role"
+                      isInvalid={!!formik.errors.role}
+                      {...formik.getFieldProps("role")}
+                    >
+                      {Object.keys(roles).map((key, index) => (
+                        <option key={index} value={roles[key]}>
+                          {key}
+                        </option>
+                      ))}
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.role}
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Email</Form.Label>
+                  <InputGroup hasValidation>
+                    <Form.Control
+                      id="email"
+                      placeholder="Enter email"
+                      isInvalid={!!formik.errors.email}
+                      {...formik.getFieldProps("email")}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {formik.errors.email}
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+              </Modal.Body>
+              <Modal.Footer className="d-flex flex-column">
+                {success || error ? (
+                  <Row>
+                    <Col className="d-flex flex-row gap-2 align-items-center">
+                      {success ? (
+                        <>
+                          <FaCheck />
+                          Operation was successful
+                        </>
+                      ) : (
+                        <>
+                          <FaExclamation />
+                          An error occured. Please try again.
+                        </>
+                      )}
+                    </Col>
+                  </Row>
+                ) : (
+                  <>
+                    {isUpdate || isDelete ? (
+                      <Row className="mb-3">
+                        <Col>
+                          {`Are you sure you wish to ${
+                            isUpdate ? "update" : "delete"
+                          } this entry?`}
+                        </Col>
+                      </Row>
+                    ) : null}
+                    <Row>
+                      <Col className="d-flex flex-row gap-4 justify-content-center align-items-center mx-auto">
+                        {isUpdate || isDelete ? (
+                          <>
+                            <Button
+                              type="submit"
+                              variant={isUpdate ? "primary" : "danger"}
+                              className={`d-flex flex-row align-items-center gap-2 ${
+                                isDelete ? "text-white" : ""
+                              }`}
+                            >
+                              {isUpdate ? (
+                                <>
+                                  <FaSync />
+                                  Update
+                                </>
+                              ) : (
+                                <>
+                                  <FaTrash />
+                                  Delete
+                                </>
+                              )}
+                            </Button>
+                            <Button
+                              variant="secondary"
+                              className="d-flex flex-row align-items-center gap-2 text-white"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                setIsUpdate(false);
+                                setIsDelete(false);
+                              }}
+                            >
+                              Cancel
+                            </Button>
+                          </>
+                        ) : (
+                          <>
+                            <Button
+                              variant="primary"
+                              className="d-flex flex-row align-items-center gap-2"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                setIsUpdate(true);
+                              }}
+                            >
+                              <FaSync />
+                              Update
+                            </Button>
+                            <Button
+                              variant="danger"
+                              className="d-flex flex-row align-items-center gap-2 text-white"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                setIsDelete(true);
+                              }}
+                            >
+                              <FaTrash />
+                              Delete
+                            </Button>
+                          </>
+                        )}
+                      </Col>
+                    </Row>
+                  </>
+                )}
+              </Modal.Footer>
+            </Form>
+          </>
+        )}
+      </Formik>
+    </Modal>
+  );
+}
+
+UserEditModal.propTypes = {
+  show: PropTypes.bool,
+  handleClose: PropTypes.func,
+  selectedEditEntry: PropTypes.object,
+  handleEdit: PropTypes.func,
+  handleDelete: PropTypes.func,
+};
 
 export default ManageUsers;

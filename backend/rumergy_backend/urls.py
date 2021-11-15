de68ca5366196d2e5ef6077a89989778ebd4c0b4
Milton Pagan <milton.pagansoto@gmail.com>
2021-11-15 16:12:03 -0400

    Add documentation with swagger

--- backend/rumergy_backend/urls.py
+++ backend/rumergy_backend/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from rumergy_backend.rumergy import views
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rumergy_backend.rumergy.views.data_log_view import DataLogViewSet
 from rumergy_backend.rumergy.views.data_point_view import DataPointViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r"api/users", views.UserViewSet, basename="users")
@@ -16,6 +19,16 @@ router.register(r"api/meter-models", views.MeterModelViewSet)
 router.register(r"api/meters", views.MeterViewSet)
 router.register(r"api/data-points", DataPointViewSet)
 router.register(r"api/data-logs", DataLogViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="RUMergy API",
+        default_version="v1",
+        description="API to support RUMergy's operations",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("", include(router.urls)),
@@ -31,4 +44,14 @@ urlpatterns = [
     ),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path(
+        r"api/docs(?P<format>\.json|\.yaml)/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        r"api/docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]

67ed37fac2d0da37a83e0501decefceac9195f0d
sebastianmerced <sebastian.merced@upr.edu>
2021-12-05 23:46:45 -0400

    Editted Modbus import. Added POST of result to API

--- backend/rumergy_backend/rumergy/views/data_log_view.py
+++ backend/rumergy_backend/rumergy/views/data_log_view.py
@@ -8,15 +8,17 @@ from rumergy_backend.rumergy.models.meter import Meter
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
-import csv
 from django.http import HttpResponse
-from datetime import datetime, timedelta
+
+from datetime import datetime, timedelta, timezone
 import requests
-from apscheduler.triggers.interval import IntervalTrigger
-import sys
-sys.path.append('/Users/sebmrcd/Desktop/rumergy-webapp/backend/modbus')
-from modbus import modbus_client as Modbus
+import csv
+from dateutil.parser import isoparse
+
+import modbus.modbus_client as Modbus
 from modbus.singleton import SchedulerHandler
+
+
 
 
 class DataLogViewSet(viewsets.ModelViewSet):
@@ -51,33 +53,39 @@ class DataLogViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         super().perform_create(serializer)
+        log_id = serializer.data['id']
         meter = serializer.data['meter']
         start_date = serializer.data['start_date']
         end_date = serializer.data['end_date']
         points = serializer.data['data_points']
         sampling = serializer.data['sampling_rate']
          
-        start_date_formated = datetime.strptime(f'{start_date}',"%Y-%m-%dT%H:%M:%SZ") 
+        # start_date_formated = datetime.strptime(f'{start_date}',"%Y-%m-%dT%H:%M:%SZ")
+        start_date_formated = isoparse(start_date)
         start_date_formated = start_date_formated + timedelta(hours=4)
-        end_date_formated = datetime.strptime(f'{end_date}',"%Y-%m-%dT%H:%M:%SZ")
+        # end_date_formated = datetime.strptime(f'{end_date}',"%Y-%m-%dT%H:%M:%SZ")
+        # end_date_formated = end_date_formated + timedelta(hours=4)
+        end_date_formated = isoparse(end_date)
         end_date_formated = end_date_formated + timedelta(hours=4)
         scheduler = SchedulerHandler().retrieve_scheduler()
 
         job = scheduler.add_job(read_points_list, trigger='interval', jobstore='djangojobstore', next_run_time=start_date_formated, seconds=sampling,
-        args=[meter, points])  
+        args=[log_id, meter, points])  
         print(job.id)
         end_job = scheduler.add_job(delete_job, trigger='date', jobstore='djangojobstore', run_date=end_date_formated, args=[job.id])
 
 
 
-def read_points_list(meter_id, points_list):
+def read_points_list(log_id, meter_id, points_list):
+    # headers={"Authorization": f"Bearer {access_token}"}
+    access_token, refresh_token = Modbus.get_token()
 
-    meter_record = requests.get(f'http://127.0.0.1:8000/api/meters/{meter_id}/').json()
+    meter_record = requests.get(f'http://127.0.0.1:8000/api/meters/{meter_id}/', headers={"Authorization": f"Bearer {access_token}"}).json()
     meter_ip = meter_record['ip']
     meter_port = meter_record['port']
 
     for point in points_list:
-        data_point = requests.get(f'http://127.0.0.1:8000/api/data-points/{point}/').json()
+        data_point = requests.get(f'http://127.0.0.1:8000/api/data-points/{point}/', headers={"Authorization": f"Bearer {access_token}"}).json()
         start_address = data_point['start_address']
         end_address = data_point['end_address']
         data_type = data_point['data_type']
@@ -87,7 +95,10 @@ def read_points_list(meter_id, points_list):
         result = Modbus.decode_message(Modbus.read_point(meter, regtype, start_address, end_address), data_type)
         meter.close()
         print(result)
-        #TODO Perform POST of result in data-log-measures
+        timestamp = datetime.now(timezone.utc)
+        log_dict = {"data_log": f"{log_id}", "data_point": f"{point}", "value": f"{result}", "timestamp": f"{timestamp}"}
+        post = requests.post('http://127.0.0.1:8000/api/data-log-measures/', headers={"Authorization": f"Bearer {access_token}"}, json=log_dict)
+        
 
 
 

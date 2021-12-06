25762d081ec946d52ac7a01ab324a9a9c73f629e
sebastianmerced <sebastian.merced@upr.edu>
2021-12-05 23:47:01 -0400

    Edited Modbus import method

--- backend/rumergy_backend/rumergy/views/live_reading_view.py
+++ backend/rumergy_backend/rumergy/views/live_reading_view.py
@@ -2,9 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 import requests
-import sys
-sys.path.append('/Users/sebmrcd/Desktop/rumergy-webapp/backend/modbus')
-from modbus import modbus_client as Modbus
+import modbus.modbus_client as Modbus
 
 @api_view(['GET'])
 @permission_classes([AllowAny])

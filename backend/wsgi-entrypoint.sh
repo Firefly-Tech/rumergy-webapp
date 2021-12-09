ad13e467fd3ee7e0324fe74033d598067b427e23
Milton Pagan <milton.pagansoto@gmail.com>
2021-12-09 10:00:27 -0500

    Add env variables to docker compose

--- backend/wsgi-entrypoint.sh
+++ backend/wsgi-entrypoint.sh
@@ -3,31 +3,22 @@
 cd /app/backend
 mkdir logs
 
+# Run migrations when db ready
 until ./manage.py migrate
 do
     echo "Waiting for db to be ready..."
     sleep 2
 done
 
+# Load fixtures
 ./manage.py loaddata initial_buildings.json
 
 gunicorn rumergy_backend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4 --access-logfile ./logs/gunicorn.log --daemon
 
 cd modbus
 
+# Start rpyc server
 python server.py &
 
+# Setup cron job
 ./cron_setup.sh
-
-#####################################################################################
-# Options to DEBUG Django server
-# Optional commands to replace abouve gunicorn command
-
-# Option 1:
-# run gunicorn with debug log level
-# gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 1 --threads 1 --log-level debug
-
-# Option 2:
-# run development server
-# DEBUG=True ./manage.py runserver 0.0.0.0:8000
-

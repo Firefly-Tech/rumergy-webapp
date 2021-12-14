e82a436bf866d33d2f8b77fe550ffd4b83dc3ac0
Milton Pagan <milton.pagansoto@gmail.com>
2021-12-14 17:34:08 -0500

    Modify cron

--- backend/modbus/cron_setup.sh
+++ backend/modbus/cron_setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+path=`pwd`
 `printenv > /etc/environment`
-(crontab -l 2>/dev/null; echo "*/15 * * * * $1/pd.sh $1 > /tmp/periodic_data.out 2>&1") | crontab -
+crontab -l > mycron
+echo "*/15 * * * * $1/pd.sh $1 > /tmp/periodic_data.out 2>&1" >> mycron
+crontab mycron
+rm mycron

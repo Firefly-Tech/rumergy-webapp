ff291d9ee5bbd6ae318fa47ba204cba9561dadb5
Milton Pagan <milton.pagansoto@gmail.com>
2021-12-18 12:35:48 -0500

    Email stuff

--- backend/rumergy_backend/settings.py
+++ backend/rumergy_backend/settings.py
@@ -146,6 +146,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_FROM= config("EMAIL_FROM")
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
